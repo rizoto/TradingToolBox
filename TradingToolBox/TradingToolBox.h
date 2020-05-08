7dcc5ee0f72e8b2115416187d5bc0768e71272b0
Lubor Kolacny <lubor@itroja.com.au>
2020-05-08 13:35:43 +1000

    Initial Commit

---
+++ TradingToolBox/TradingToolBox.h
@@ -0,0 +1,19 @@
+//
+//  TradingToolBox.h
+//  TradingToolBox
+//
+//  Created by Lubor Kolacny on 8/5/20.
+//  Copyright © 2020 Lubor Kolacny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TradingToolBox.
+FOUNDATION_EXPORT double TradingToolBoxVersionNumber;
+
+//! Project version string for TradingToolBox.
+FOUNDATION_EXPORT const unsigned char TradingToolBoxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TradingToolBox/PublicHeader.h>
+
+
